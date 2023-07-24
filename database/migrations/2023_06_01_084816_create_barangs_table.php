0065d3780a8656df5d9c83e93152b0219c021124
daffaari <daffasaharii@gmail.com>
2023-07-24 23:50:16 +0700

    fix error migration

--- database/migrations/2023_06_01_084816_create_barangs_table.php
+++ database/migrations/2023_06_01_084816_create_barangs_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('nama_barang', 100);
             // $table->integer('stock');
             $table->string('merk', 50);
-            $table->integer('id_supplier', 11);
+            $table->unsignedBigInteger('id_supplier');
             $table->string('kategori', 50);
             $table->timestamps();
         });
